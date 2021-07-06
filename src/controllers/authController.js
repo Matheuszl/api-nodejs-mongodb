2f8e4cbc8e4bc4d289b3ff05b67e09628c93f7b4
Matheuszl <matheus.zzalamena@gmail.com>
2021-07-05 22:36:14 -0300

    Fix mongoDB, add bcrypt for password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,11 +4,17 @@ const User = require('../models/User');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
-  try{
-    const user = await User.create(req.body);
-  
-    return res.send({ user });
+  const { email } = req.body;
 
+  try{
+    if (await User.findOne({ email }))
+      return res.status(400).send({ error: 'User already exists'});
+
+    const user = await User.create(req.body);
+
+    user.password = undefined;
+
+    return res.send({ user });
   } catch (err) {
     return res.status(400).send({ error: 'Registration failed'});
   }
