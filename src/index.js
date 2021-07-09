c9c8a067cca9b328ce5ee9740e553de5bd306f22
Matheuszl <matheus.zzalamena@gmail.com>
2021-07-08 22:06:57 -0300

    Auth 0.2 / JWT 0.2

--- src/index.js
+++ src/index.js
@@ -10,5 +10,6 @@ app.use(express.urlencoded({ extended: true}));
 
 //repasso o app
 require('./controllers/authController')(app);
+require('./controllers/projectController')(app);
 //controllers\authController.js
 app.listen(3000);
