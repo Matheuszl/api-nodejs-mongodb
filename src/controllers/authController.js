5eba8a5b51a167e43937f160e38d71764ae3fa21
Matheuszl <matheus.zzalamena@gmail.com>
2021-07-08 21:04:32 -0300

    JWT 0.1

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const auth = require('../config/auth');
+
 const router = express.Router();
+
+function genereteToken(params = {}){
+  return jwt.sign(params, auth.secret, {
+    expiresIn: 86400,
+  });
+}
+
 
 router.post('/register', async (req, res) => {
   const { email } = req.body;
@@ -14,7 +24,11 @@ router.post('/register', async (req, res) => {
 
     user.password = undefined;
 
-    return res.send({ user });
+    return res.send({ 
+      user,
+      token: genereteToken({ id: user.id }),
+    });
+    
   } catch (err) {
     return res.status(400).send({ error: 'Registration failed'});
   }
@@ -32,7 +46,22 @@ router.post('/authenticate', async (req, res) => {
   if(!await bcrypt.compare(password,user.password))
     return res.status(400).send({ error: 'Invalid password'});
 
-  res.send({ user });
+  user.password = undefined;
+  /**
+   * como se fosse entrar/login
+   * preciso passar a info que diferencia um token de outro
+   * e essa info é o ID sempre unico
+   * user.id
+   * 
+   * o segundo parametro e o hash que tambem é unico
+   * 
+   * o terceiro é o tempo de expiração em segundos
+   */
+
+  res.send({ 
+    user,
+    token: genereteToken({ id: user.id }),
+  });
 });
 
 module.exports = app => app.use('/auth', router);
