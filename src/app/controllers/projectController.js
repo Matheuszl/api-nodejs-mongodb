98a4571e10f4a7c0e97213e753d085b4db6ef4e8
Matheuszl <matheus.zzalamena@gmail.com>
2021-07-21 23:32:42 -0300

    Update Project controller: GET, POST, DELETE

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -11,24 +11,67 @@ router.use(authMiddleware)
 
 //rota que retorna todos projetos
 router.get('/', async (req, res) => {
-  res.send({ user: req.userId });
+  try {
+    //apenas esse metodo find() ja retorna todos os projetos no bd
+    const projects = await Project.find().populate('user');
+
+    return res.send({ projects });
+
+  }catch(err){
+    return res.status(400).send({ error: 'error loading projects' });
+  }
 });
 
-router.get('/projectId', async (req, res) => {
-  res.send({ user: req.userId });
+//rota de findbyid busca por um projeto por id
+router.get('/:projectId', async (req, res) => {
+  
+  try {
+    const project = await Project.findById(req.params.projectId).populate('user');
+
+    return res.send({ project });
+
+  }catch(err){
+    return res.status(400).send({ error: 'error loading project' });
+  }
+
 });
 
 //rota de create de um novo projeto
 router.post('/', async (req, res) => {
-  res.send({ user: req.userId });
+  try {
+    //usar o await pq isso é uma consulta ao mongoose e ele retorna uma promisse
+    //o user: vai buscar qual usuario criou o projeto, quem preenche é o midware de auth
+    const project = await Project.create({...req.body, user: req.userId});
+    
+
+
+    //retorna o projeto
+    return res.send({ project });
+
+
+  } catch (error) {
+    return res.status(400).send({ error: 'error crating new project'});
+  }
 });
 
-router.put('/projectId', async (req, res) => {
-  res.send({ user: req.userId });
+//rota de atualizaçao
+router.put('/:projectId', async (req, res) => {
+
 });
 
-router.delete('/projectId', async (req, res) => {
-  res.send({ user: req.userId });
+//rota de delete
+router.delete('/:projectId', async (req, res) => {
+  try {
+    await Project.findByIdAndRemove(req.params.projectId);
+
+    //retorno apenas o send pq ja excluiu, 200=ok
+    return res.send();
+
+  }catch(err){
+    return res.status(400).send({ error: 'error deleting project' });
+  }
 });
 
 module.exports = app => app.use('/projects', router);
+
+//15.45 metodo de update e post com as task
