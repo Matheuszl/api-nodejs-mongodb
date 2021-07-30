749d1333aec9edb904f2f6fa1374bd534b7d73cb
Matheuszl <matheus.zzalamena@gmail.com>
2021-07-30 15:10:09 -0300

    PDFPrinter 0.3 | presente o bug do createdAt

--- src/app/controllers/pdfController.js
+++ src/app/controllers/pdfController.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get('/projetos', async (req, res) => {
   try {
     //apenas esse metodo find() ja retorna todos os projetos no bd
-    const projects = await Project.find();
+    const projects = await Project.find().populate(['user']);
     // Define font files
     const fonts = {
       Helvetica: {
@@ -24,23 +24,30 @@ router.get('/projetos', async (req, res) => {
     const printer = new PDFPrinter(fonts);
     const body = [];
 
+    //nesse for varremos oque buscamos do banco de dados e colomos em um array apenas oque queremos
     for await (let project of projects) {
       const rows = new Array();
+      rows.push(project.id);
       rows.push(project.title);
       rows.push(project.description);
-
+      rows.push(project.user.name);
+      //rows.push(project.user.email);
+      rows.push(project.createdAt);
+  
       body.push(rows);
     }
 
     const docDefinition = {
       defaultStyle: {
-        font: 'Helvetica'
+        font: 'Helvetica' //muito importante, se tirar isso o coigo da pau
       },
       content: [
+        { text: 'PROJETOS', style: 'header' },
+        'Relatorio de projetos atuais em desenvolvimento',
         {
           table: {
             body: [
-              ['title', 'description'], ...body],
+              ['ID','Titulo', 'Descrição', 'Usuario', 'Data emissão'], ...body],
           },
         },
       ],
